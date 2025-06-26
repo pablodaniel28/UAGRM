2b109d8c202d9a4237106ea9acf2e8fd241aba55
pablodaniel28 <117131469+pablodaniel28@users.noreply.github.com>
2025-06-26 12:59:35 -0400

    nuevo codigo

--- src/main/java/com/phegondev/usersmanagementsystem/repository/ManagementRepo.java
+++
@@ -1,9 +0,0 @@
-package com.phegondev.usersmanagementsystem.repository;
-
-import com.phegondev.usersmanagementsystem.entity.managementdb;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManagementRepo extends JpaRepository<managementdb, Integer> {
-}
